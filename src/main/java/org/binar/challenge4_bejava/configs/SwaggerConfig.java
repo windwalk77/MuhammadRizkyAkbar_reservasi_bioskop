269992d6aefd5660fda8e447dd4a32fb7be6247d
windwalk77 <rizkya7754@gmail.com>
2022-10-21 21:59:03 +0700

    java

--- src/main/java/org/binar/challenge4_bejava/configs/SwaggerConfig.java
+++ src/main/java/org/binar/challenge4_bejava/configs/SwaggerConfig.java
@@ -2,8 +2,12 @@ package org.binar.challenge4_bejava.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
@@ -13,7 +17,8 @@ public class SwaggerConfig {
                             .title("Aplikasi Bioskop")
                             .description("REST API Bioskop - Muhammad Rizky Akbar")
                             .version("0.2")
-                    );
+
+                    ).servers(List.of(new Server().url("https://reservasi-bioskop-rizkyakbar.up.railway.app/")));
         }
 
 
